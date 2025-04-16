79af8841c8510b208dc363f3aa6940f433903097
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-16 16:32:14 +0100

    Update MainLayout.jsx

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,18 +1,25 @@
+// src/components/MainLayout.jsx
 import React from 'react';
 import Navbar from './Navbar';
+import LanguageSwitcher from './LanguageSwitcher';
 import ThemeToggle from './ThemeToggle';
+import Footer from './Footer';
 
 const MainLayout = ({ children }) => {
   return (
-    <>
+    <div>
       <Navbar />
-      <div className="px-4 py-2">
+      <main className="px-4 py-2">
         {children}
+      </main>
+      <div className="fixed bottom-4 left-4">
+        <LanguageSwitcher />
       </div>
       <div className="fixed bottom-4 right-4">
         <ThemeToggle />
       </div>
-    </>
+      <Footer />
+    </div>
   );
 };
 
