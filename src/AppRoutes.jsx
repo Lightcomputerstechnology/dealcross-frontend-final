742db9471bce56fc1aa868d2125a101138d10294
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-16 05:31:23 +0100

    Update AppRoutes.jsx

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage.jsx';
-import LoginPage from './pages/LoginPage.jsx';
-import SignupPage from './pages/SignupPage.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './LandingPage.jsx';
+import SignupPage from './SignupPage.jsx';
+import LoginPage from './LoginPage.jsx';
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
   );
 };
 
