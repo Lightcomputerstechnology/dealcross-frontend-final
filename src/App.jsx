8a0f9f1fa0cf7d3d7525d51af6f74d76be681469
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-16 04:55:23 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import ThemeToggle from "./ThemeToggle";
 
 function App() {
   return (
     <BrowserRouter>
-      <AppRoutes />
+      <div className="min-h-screen flex flex-col bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-100">
+        <Navbar />
+        <main className="flex-grow px-4 py-6">
+          <AppRoutes />
+        </main>
+        <Footer />
+        <ThemeToggle />
+      </div>
     </BrowserRouter>
   );
 }
