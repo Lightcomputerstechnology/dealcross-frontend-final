7a8c26d00e64db519bc3b3a98c8ffa2f64bf78e4
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-16 05:47:29 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="bg-black text-white px-6 py-3 flex justify-between items-center">
+    <nav className="bg-gray-900 text-white px-6 py-4 flex justify-between items-center">
       <h1 className="text-xl font-bold">Dealcross</h1>
       <div className="space-x-4">
-        <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Signup</Link>
+        <Link to="/" className="hover:text-blue-400">Home</Link>
+        <Link to="/login" className="hover:text-blue-400">Login</Link>
+        <Link to="/signup" className="hover:text-blue-400">Signup</Link>
       </div>
     </nav>
   );
