ebb37bf72e6cf360c696060f0b53c83b9b1b346a
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-16 08:58:36 +0100

    Update AppRoutes.jsx

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -5,6 +5,7 @@ import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import PitchDeckViewer from './pages/PitchDeckViewer';
+import FraudDetectionLog from './pages/FraudDetectionLog';
 
 const AppRoutes = () => {
   return (
@@ -13,6 +14,7 @@ const AppRoutes = () => {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/pitch-deck" element={<PitchDeckViewer />} />
+      <Route path="/fraud-log" element={<FraudDetectionLog />} />
     </Routes>
   );
 };
