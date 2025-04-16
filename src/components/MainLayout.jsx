acf5a2f3ab724fb3102b65ec4e145b51d2137042
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-16 15:59:26 +0100

    Update MainLayout.jsx

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './Navbar';
+import ThemeToggle from './ThemeToggle';
 
 const MainLayout = ({ children }) => {
   return (
@@ -7,6 +8,9 @@ const MainLayout = ({ children }) => {
       <Navbar />
       <div className="px-4 py-2">
         {children}
+      </div>
+      <div className="fixed bottom-4 right-4">
+        <ThemeToggle />
       </div>
     </>
   );
