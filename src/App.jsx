9752ea1309e019a79c6158936f1a3079d473bba4
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-18 00:18:05 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
 // src/App.jsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import LandingPage from './pages/LandingPage';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-import StartDealPairing from './pages/StartDealPairing';
-import ShareTrading from './pages/ShareTrading';
+import LandingPage from './pages/LandingPage'
+import ShareTradingPage from './pages/ShareTradingPage'
+import LoginPage        from './pages/LoginPage'
+import SignUpPage       from './pages/SignUpPage'
+// …etc
 
-function App() {
+export default function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/pair-deal" element={<StartDealPairing />} />
-            <Route path="/share" element={<ShareTrading />} />
-          </Routes>
-        </main>
-
-        <Footer />
-      </div>
+      <Routes>
+        {/* ← this is the magic: path="/" must render your LandingPage */}
+        <Route path="/"            element={<LandingPage />}     />
+        <Route path="/share-trading" element={<ShareTradingPage />} />
+        <Route path="/login"       element={<LoginPage />}       />
+        <Route path="/signup"      element={<SignUpPage />}      />
+        {/* …other routes */}
+      </Routes>
     </Router>
-  );
+  )
 }
-
-export default App;
