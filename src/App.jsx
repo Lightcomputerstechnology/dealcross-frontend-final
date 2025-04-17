c2beba46cc114781b6936ba93cb14049c6988a8c
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-17 03:22:13 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,23 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import AppRoutes from "./AppRoutes";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import NotificationToast from "./components/NotificationToast";
-import ThemeToggle from "./components/ThemeToggle";
+import React from 'react';
+import AppRoutes from './AppRoutes';
 
-const App = () => {
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import ThemeToggle from './components/ThemeToggle';
+import LanguageSwitcher from './components/LanguageSwitcher';
+import NotificationToast from './components/NotificationToast';
+
+function App() {
   return (
-    <Router>
-      <div className="min-h-screen flex flex-col bg-background text-foreground transition-colors duration-300">
-        {/* Header */}
-        <header className="w-full shadow-md z-50">
-          <Navbar />
-        </header>
-
-        {/* Main Routes */}
-        <main className="flex-grow">
-          <AppRoutes />
-        </main>
-
-        {/* Footer */}
-        <footer className="mt-10">
-          <Footer />
-        </footer>
-
-        {/* Utility Components */}
-        <ThemeToggle />
-        <NotificationToast />
-      </div>
-    </Router>
+    <>
+      <Navbar />
+      <AppRoutes />
+      <LanguageSwitcher />
+      <ThemeToggle />
+      <NotificationToast />
+      <Footer />
+    </>
   );
-};
+}
 
 export default App;
