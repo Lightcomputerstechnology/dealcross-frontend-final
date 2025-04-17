64f5812c938787b46906130bf6e89c45fd7fc3b6
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-17 18:36:36 +0100

    Update card.jsx

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -6,3 +6,11 @@ export function Card({ children, className }) {
     </div>
   );
 }
+
+export function CardContent({ children, className }) {
+  return (
+    <div className={`p-2 ${className || ""}`}>
+      {children}
+    </div>
+  );
+}
