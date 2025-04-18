c5f45304b2182d38ddb6a5e042f4e985dcc45210
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-18 05:46:49 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
-// src/App.jsx
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
-import LandingPage from './pages/LandingPage'
-import ShareTradingPage from './pages/ShareTradingPage'
-import LoginPage        from './pages/LoginPage'
-import SignUpPage       from './pages/SignUpPage'
-// …etc
+-import ShareTradingPage  from './pages/ShareTradingPage'
++import ShareTrading      from './pages/ShareTrading'
 
 export default function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        {/* ← this is the magic: path="/" must render your LandingPage */}
-        <Route path="/"            element={<LandingPage />}     />
-        <Route path="/share-trading" element={<ShareTradingPage />} />
-        <Route path="/login"       element={<LoginPage />}       />
-        <Route path="/signup"      element={<SignUpPage />}      />
-        {/* …other routes */}
+        <Route path="/"              element={<LandingPage/>}   />
+-       <Route path="/share-trading" element={<ShareTradingPage/>}/>
++       <Route path="/share-trading" element={<ShareTrading/>}   />
+        {/* …other routes… */}
       </Routes>
-    </Router>
+    </BrowserRouter>
   )
 }
