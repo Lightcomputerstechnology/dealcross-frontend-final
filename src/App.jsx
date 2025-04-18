3bbf6a09a77908bf13698c43d860a65bf5086ec0
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-18 06:04:58 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 // src/App.jsx
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import LandingPage  from './pages/LandingPage';
 import Deals        from './pages/Deals';
