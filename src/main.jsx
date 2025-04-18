3b062b7baa514cfee4727ff16ffbb5e44cccc5c6
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-18 07:01:27 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './index.css'   // your Tailwind / CSS entry
+import './index.css'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root')
-)
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
