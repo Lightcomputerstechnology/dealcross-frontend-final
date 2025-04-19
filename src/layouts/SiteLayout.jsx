cb55b73ffff1835994a0b9f8ef21909ba3228877
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-19 09:55:10 +0100

    Update SiteLayout.jsx

--- src/layouts/SiteLayout.jsx
+++ src/layouts/SiteLayout.jsx
@@ -3,15 +3,25 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Logo from "@/assets/dealcross-logo.png";
 
 const SiteLayout = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-white dark:bg-gray-900 transition duration-300">
-      <Navbar />
-      <main className="flex-grow">
-        <Outlet />
-      </main>
-      <Footer />
+    <div className="relative flex flex-col min-h-screen bg-white dark:bg-gray-900 transition duration-300 overflow-hidden">
+      {/* Global watermark background */}
+      <img
+        src={Logo}
+        alt="Watermark"
+        className="absolute inset-0 w-full h-full object-contain opacity-5 pointer-events-none z-0"
+      />
+
+      <div className="relative z-10 flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 };
