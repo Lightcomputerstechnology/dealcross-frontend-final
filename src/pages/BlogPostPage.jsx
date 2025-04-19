502a62a9af25ada60e87e0f549603994a2541154
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-19 18:18:38 +0100

    Create BlogPostPage.jsx

---
+++ src/pages/BlogPostPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import MarkdownPost from '@/components/MarkdownPost';
+
+const BlogPostPage = () => {
+  return <MarkdownPost filename="intro-to-dealcross.md" />;
+};
+
+export default BlogPostPage;
