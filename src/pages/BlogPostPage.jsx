6753be817925c0e21e0cdf06ac6e1cf0d7e232e6
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-19 19:10:43 +0100

    Delete src/pages/BlogPostPage.jsx

--- src/pages/BlogPostPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import MarkdownPost from '@/components/MarkdownPost';
-
-const BlogPostPage = () => {
-  return <MarkdownPost filename="intro-to-dealcross.md" />;
-};
-
-export default BlogPostPage;
