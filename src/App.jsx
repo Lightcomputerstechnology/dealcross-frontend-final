76ff83cc80e68ec1875799d3cba913a91c9cf9dc
Lightcomputerstechnology <154016572+Lightcomputerstechnology@users.noreply.github.com>
2025-04-21 20:23:42 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,24 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
 import './index.css'; // Tailwind/global styles
 import { Toaster } from 'react-hot-toast';
+import NotificationAlert from './components/NotificationAlert';
+import { NotificationProvider } from './context/NotificationContext';
 
 export default function App() {
   return (
     <Router>
-      <AppRoutes />
-      <Toaster position="top-right" reverseOrder={false} />
+      <NotificationProvider>
+        <AppRoutes />
+        <NotificationAlert />
+        <Toaster
+          position="top-right"
+          reverseOrder={false}
+          toastOptions={{
+            className: 'bg-white dark:bg-gray-900 text-gray-900 dark:text-white shadow-xl',
+            duration: 4000,
+          }}
+        />
+      </NotificationProvider>
     </Router>
   );
 }
